c146f1959830bc30d180fae73a6c229dcf9b247c
ugromate <ugromate@stud.uni-obuda.hu>
2021-10-10 00:17:54 +0200

    Correcting naming

---
+++ XCWED2_HFT_2021221.Client/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using XCWED2_HFT_2021221.Repository.DbContexts;
+
+namespace XCWED2_HFT_2021221.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Started");
+            var context = new XCWED2_HFT_2021221DbContexts();
+            Console.WriteLine("Connected");
+
+
+            //foreach (var boargame in context.BoardGames)
+            //{
+            //    Console.WriteLine(boargame.Name);
+            //}
+
+            Console.WriteLine();
+
+            foreach (var boardgame in context.BoardGames)
+            {
+                Console.WriteLine($"Id: {boardgame.Id}, Publisher: {boardgame.Publisher.Name} - Title: {boardgame.Name} - Designer: {boardgame.Designer.Name}");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
